ef2168893a34654466a3aecd4fb8fddf7a8d2cef
geovannybolang <hezkiebolang@gmail.com>
2023-05-11 03:57:35 +0800

    UpdateUI

--- iot/script.js
+++ iot/script.js
@@ -17,5 +17,14 @@ $(document).ready(function() {
           $("#gas").text(gas);
       });
     }, 1000); // Set interval 1 detik
+    setInterval(function() {
+      $.get("https://skripsi-e4a2a-default-rtdb.firebaseio.com/Hasil_Pembacaan/hum1.json", function(data) {
+          var humidity = "";
+          for (var i = 0; i < data.length; i++) {
+              gas += data[i];
+          }
+          $("#humidity").text(humidity);
+      });
+    }, 1000); // Set interval 1 detik
   });
   
