428a9337072fbe52d3564bc544c7fbb259ca86c8
geovannybolang <hezkiebolang@gmail.com>
2023-05-11 00:44:47 +0800

    Update_suhu

--- iot/script.js
+++ iot/script.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
-    setInterval(function() {
+  setInterval(function() {
       $.get("https://skripsi-e4a2a-default-rtdb.firebaseio.com/Hasil_Pembacaan/suhu1.json", function(data) {
-        var suhu = data[0];
-        $("#suhu").text(suhu);
+          var suhu = "";
+          for (var i = 0; i < data.length; i++) {
+              suhu += data[i];
+          }
+          $("#suhu").text(suhu);
       });
-    }, 1000); // Set interval 1 detik
-  
+  }, 1000);
     setInterval(function() {
       $.get("http://iot.serangkota.go.id:8080/HTDXmVIHWa2bw_JvErBjzbwKiE9tqilB/get/V3", function(data) {
         var kelembaban = data[0];
