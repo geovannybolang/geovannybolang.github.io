428dcff390c888e2dd96cc0ce4840bde0d91ebd1
geovannybolang <hezkiebolang@gmail.com>
2023-07-13 17:42:49 +0800

    Remove Gas

--- iot/script.js
+++ iot/script.js
@@ -8,14 +8,6 @@ $(document).ready(function() {
           $("#suhu").text(suhu);
       });
   }, 1000);
-    setInterval(function() {
-      $.get("https://skripsi-e4a2a-default-rtdb.firebaseio.com/Hasil_Pembacaan/gas1.json", function(data) {
-          var gas = "";
-          for (var i = 0; i < data.length; i++) {
-              gas += data[i];
-          }
-          $("#gas").text(gas);
-      });
     }, 1000); // Set interval 1 detik
     setInterval(function() {
       $.get("https://skripsi-e4a2a-default-rtdb.firebaseio.com/Hasil_Pembacaan/hum1.json", function(data) {
