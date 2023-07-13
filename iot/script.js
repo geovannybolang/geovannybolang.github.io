5db08b1995b6a38ee6397cd2a264e8fcae902f46
geovannybolang <hezkiebolang@gmail.com>
2023-07-13 18:07:23 +0800

    Change directory again

--- iot/script.js
+++ iot/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   setInterval(function() {
-      $.get("https://skripsi-e4a2a-default-rtdb.firebaseio.com/Hasil_Pembacaan/suhu1.json", function(data) {
+      $.get("https://skripsi-e4a2a-default-rtdb.firebaseio.com/Hasil_Pembacaan/temperature.json", function(data) {
           var suhu = "";
           for (var i = 0; i < data.length; i++) {
               suhu += data[i];
@@ -10,7 +10,7 @@ $(document).ready(function() {
   }, 1000);
     }, 1000); // Set interval 1 detik
     setInterval(function() {
-      $.get("https://skripsi-e4a2a-default-rtdb.firebaseio.com/Hasil_Pembacaan/hum1.json", function(data) {
+      $.get("https://skripsi-e4a2a-default-rtdb.firebaseio.com/Result/humidity.json", function(data) {
           var humidity = "";
           for (var i = 0; i < data.length; i++) {
               gas += data[i];
